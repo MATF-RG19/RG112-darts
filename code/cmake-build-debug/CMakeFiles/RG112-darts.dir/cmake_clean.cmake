12170b8c50e5917029539ff0519139d2cdf93bda
dimitrije <dimitrijestankov98@gmail.com>
2019-12-26 18:25:29 +0100

    Added cmake-build-debug

---
+++ code/cmake-build-debug/CMakeFiles/RG112-darts.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RG112-darts.dir/main.cpp.o"
+  "RG112-darts"
+  "RG112-darts.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RG112-darts.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
